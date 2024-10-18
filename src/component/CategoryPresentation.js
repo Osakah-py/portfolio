ace6ab19eee875b5857300726424ad68058dce42
alex <lescot.alexg@gmail.com>
2024-10-18 23:42:08 +0200

    ajout fondu Header + Categories responsives

--- src/component/CategoryPresentation.js
+++ src/component/CategoryPresentation.js
@@ -10,15 +10,16 @@ function CategoryPresentation() {
             <Flex
                 wrap="wrap"
                 justify="between"
+                align="stretch"
             >
                 <Flex
                     direction="column"
                     justify="center"
                     gap="4"
-                    style={{"width":"50%"}}
+                    className="CatPresImage"
                     >
                     <Heading as="h1" size="8">My Projects</Heading>
-                    <Text>From simple interfaces to complex web interfaces, during my two years at ENSEEIHT I had the opportunity to work on a bunch of different computer science.  </Text>
+                    <Text>From simple interfaces to complex web interfaces, during my two years at ENSEEIHT I had the opportunity to work on a bunch of different computer science projects.  </Text>
                     <Button variant='surface' style={{"max-width":"150px", "padding":"20px 5px"}}>View All My Projects</Button>
                 </Flex>
                 <img className="CatPresImage" src="https://framerusercontent.com/images/eSSOryz6R0dgWVPDhxZuRb7oV5E.gif" />
